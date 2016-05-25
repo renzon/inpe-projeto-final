ac394e624a6b6ecfd7f75e556f0d5c4b3739cb65
renzon <renzon@gmail.com>
2016-05-24 23:56:37 -0300

    Arquivos iniciais da tarefa

---
+++ src/tarefa2/ValidadorTarefa2.java
@@ -0,0 +1,11 @@
+package tarefa2;
+
+import org.esfinge.aom.api.model.IEntity;
+
+public class ValidadorTarefa2 {
+	
+	public boolean validarEntidadeCobranca(IEntity cobranca) throws Exception{
+		return true;
+	}
+
+}
